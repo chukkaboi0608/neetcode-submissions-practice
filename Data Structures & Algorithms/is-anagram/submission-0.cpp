b52b08a93a3201e48fdc471dbeb2e23edfe21051
chukkaboi0608 <151138823+chukkaboi0608@users.noreply.github.com>
2026-04-05 16:19:19 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        map<char,int> mpp;
+ 
+        for(int i =0; i < s.length(); i++)
+        {
+            mpp[s[i]]++;
+            mpp[t[i]]--;
+        }
+        for(auto &p : mpp){
+            if(p.second != 0)
+            return false;
+        }
+        return true;
+    }
+
+};
